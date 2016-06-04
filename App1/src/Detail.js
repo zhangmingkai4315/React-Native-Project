c5d52a2f53a500b0b02912ce6366ccf145c5828c
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2016-06-04 10:09:31 +0800

    修改search的行为 尚未处理select方法

---
+++ App1/src/Detail.js
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+const Detail = React.createClass({
+  render () {
+    return (
+
+    )
+  }
+})
+
+export default Detail
--- App1/src/Search.js
+++ App1/src/Search.js
@@ -4,21 +4,72 @@ import {
   Text,
   View,
   TextInput,
+  PixelRatio,
 } from 'react-native';
-class Search extends Component {
-  render () {
+const onePT=1/PixelRatio.get();
+const Search = React.createClass({
+  getInitialState:function(){
+   return {
+      show:false,
+      inputValue:'',
+    };
+  },
+  getValue:function(text){
+    this.setState({
+      show:true,
+      inputValue:text
+    })
+  },
+  hide:function(text){
+    this.setState({
+      show:false,
+      inputValue:text
+    })
+  },
+  selectItem:function(text){
+    console.log(text+" in select")
+    this.setState({
+      show:false,
+      inputValue:text
+    });
+  },
+  render :function() {
+    let helperText='';
+
+    if(this.state.show){
+      const renderHelper=['酒店','度假村','宾馆'].map(function(item,i){
+          let itemString=this.state.inputValue+item;
+          console.log(itemString)
+          return <Text style={styles.item} key={i} numberOfLines={1} onPress={this.selectItem.bind(this,itemString)}>
+          {itemString}</Text>
+      }.bind(this));
+
+      helperText=<View style={[styles.result]}>{renderHelper}</View>;
+   }else{
+      helperText=<View></View>
+   }
     return (
+      <View style={styles.flex}>
       <View style={[styles.container]}>
         <View style={styles.flex}>
-          <TextInput style={styles.input} returnKeyType="search"/>
+          <TextInput placeholder="请输入旅行目的地"
+            onEndEditing={this.hide.bind(this,this.state.inputValue)}
+            style={styles.input}
+            returnKeyType="search"
+            value={this.state.inputValue}
+            onChangeText={this.getValue}
+          />
         </View>
         <View style={styles.btn}>
-            <Text style={styles.search}>搜索</Text>
+            <Text style={styles.search} onPress={this.hide.bind(this,this.state.inputValue)}>搜索</Text>
         </View>
+
+      </View>
+      {helperText}
       </View>
     )
   }
-}
+});
 
 
 const styles=StyleSheet.create({
@@ -39,6 +90,23 @@ const styles=StyleSheet.create({
     paddingLeft:5,
     borderColor:"#ccc",
     borderRadius:4
+  },
+  result:{
+    marginTop:onePT,
+    marginLeft:5,
+    marginRight:5,
+    height:200,
+    borderColor:'#ccc',
+    borderTopWidth:onePT,
+  },
+  item:{
+    fontSize:16,
+    padding:5,
+    paddingTop:10,
+    paddingBottom:10,
+    borderWidth:onePT,
+    borderColor:'#ccc',
+    borderTopWidth:0,
   },
   btn:{
     width:55,
